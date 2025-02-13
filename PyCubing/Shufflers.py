4134839eff9cc91b5af033a615700aeb6627ef35
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-02-13 11:23:26 -0300

    Add 2x2x2 Scrambler

--- PyCubing/Shufflers.py
+++ PyCubing/Shufflers.py
@@ -1,17 +1,22 @@
 from random import randint
 
-def Cube2x2x2() -> list:
+def Cube2x2x2(size: int = 12, ANSI: bool = True) -> list:
   """
     This function generates a scramble for 2x2x2 Cube
-    Following the WCA guidelines
+    Following the WCA guidelines.
   """
 
-  frases: tuple = (
-    'It\'s working!!!',
-    'It\'s really working!',
-    'Oh! It\'s working...',
-  )
-  return [frases[randint(0, 2)]]
+  Moves_types: tuple = ('R', 'U', 'F')
+  Orientation: tuple = ("", "'", "2")
+
+  Moves = []
+  for i in range(1, size + 1):
+    Moves.append(
+      Moves_types[randint(0, 2)] +
+      Orientation[randint(0, 2)]
+    )
+  
+  return Moves
 
 if __name__ == "__main__":
   print("""
--- Testing.py
+++ Testing.py
@@ -2,9 +2,12 @@ import PyCubing
 from PyCubing import Shufflers
 
 def main():
+    # Test 2x2x2 Scramble
+    print('\033[1;4m2x2x2 Scramble:\033[m \033[3m', end='')
     gen2x2x2: list = Shufflers.Cube2x2x2()
     for item in gen2x2x2:
-        print(item)
+        print(item, end=' ')
+    print('\033[m')
 
 
 if __name__ == "__main__":
