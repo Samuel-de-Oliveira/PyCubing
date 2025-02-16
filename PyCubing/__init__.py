61053de150d664939e4ce0d8c2a68d74d0bc760b
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-02-15 22:32:51 -0300

    Add PEP8 in th project

--- PyCubing/__init__.py
+++ PyCubing/__init__.py
@@ -1,12 +1,10 @@
 from prettytable import PrettyTable
 
 __version__: str = '0.1'
-__author__:  str = 'Samuel de Oliveira'
+__author__: str = 'Samuel de Oliveira'
 __license__: str = 'MIT'
 
 __all__: list = []
 
-if __name__ == "__main__":
-  print("""
-    This is a module file, then you should import it.
-  """)
+if __name__ == '__main__':
+    print("""This is a module file, then you should import it.""")
