a105e8f6e53d989c12dbc5f360cf51a6188f6fda
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-02-18 10:31:00 -0300

    Add WCA API

--- PyCubing/__init__.py
+++ PyCubing/__init__.py
@@ -4,17 +4,49 @@ __version__: str = '0.1'
 __author__: str = 'Samuel de Oliveira'
 __license__: str = 'MIT'
 
-__all__: list = []
+__all__: list = [
+    'get_wca_rankig',
+    'API_MODALITIES',
+]
+
+API_MODALITIES: dict = {
+    '2x2x2': '222',
+    '3x3x3': '333',
+    '4x4x4': '444',
+    '5x5x5': '555',
+    '6x6x6': '666',
+    '7x7x7': '777',
+    '3x3x3bf': '333bf',
+    '3x3x3mbf': '333mbf',
+    '3x3x3oh': '333oh',
+    '3x3x3ft': '333ft',
+    '4x4x4bf': '444bf',
+    '5x5x5bf': '555bf',
+    'clock': 'clock',
+    'megaminx': 'minx',
+    'pyraminx': 'pyram',
+    'skewb': 'skewb',
+    'square_one': 'sq1',
+}
 
 # This API is powered by Robin Ingelbrecht at https://github.com/robiningelbrecht/wca-rest-api
-WCA_UNOFFICIAL_API: str =  "https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/"
+WCA_UNOFFICIAL_API: str = 'https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api'
 
 
-def get_wca_ranking(modality: str, continent:str, ) -> None:
-  pass
+def get_wca_ranking(
+    event: str = '3x3x3',
+    region: str = 'world',
+    score_type: str = 'average',
+    size: int = 10,
+) -> None:
+    get_ranking = requests.get(
+        f'{WCA_UNOFFICIAL_API}/rank/{region}/{score_type}/{API_MODALITIES[event]}.json'
+    )
+    for person in range(0, size):
+        print(
+            f"\033[1;34m{person + 1}\033[m - \033[1m{get_ranking.json()['items'][person]['personId']}:\033[m {get_ranking.json()['items'][person]['best']}"
+        )
 
 
-if __name__ == "__main__":
-  print("""
-    This is a module file, then you should import it.
-  """)
+if __name__ == '__main__':
+    print('This is a module file, then you should import it.')
--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='PyCubing',
     version='0.1',
     packages=find_packages(),
-    install_requires=['prettytable'],
+    install_requires=['requests'],
 )
