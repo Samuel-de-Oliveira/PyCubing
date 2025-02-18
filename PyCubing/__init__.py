13284992515f68f029f47bd80f594cf099a01bf7
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-02-18 09:37:10 -0300

    .

--- PyCubing/__init__.py
+++ PyCubing/__init__.py
@@ -1,10 +1,18 @@
-from prettytable import PrettyTable
+import requests
 
 __version__: str = '0.1'
 __author__:  str = 'Samuel de Oliveira'
 __license__: str = 'MIT'
 
 __all__: list = []
+
+# This API is powered by Robin Ingelbrecht at https://github.com/robiningelbrecht/wca-rest-api
+WCA_UNOFFICIAL_API: str =  "https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/"
+
+
+def get_wca_ranking(modality: str, continent:str, ) -> None:
+  pass
+
 
 if __name__ == "__main__":
   print("""
