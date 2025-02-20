dd4dd7555034f9dcc3f73680f32e5786a505ae4c
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-02-20 11:08:31 -0300

    .

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    package_dir={'': 'PyCubing'},
+    package_dir={'PyCubing': 'PyCubing'},
     packages=find_packages(where='PyCubing'),
     python_requires='>=3.6',
     install_requires=[
